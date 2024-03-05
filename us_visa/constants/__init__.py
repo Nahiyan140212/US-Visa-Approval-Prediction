9c06780bf2f952a21f9fd447d05d9b4187a1d99f
Nahiyan140212 <nbnoor@ualr.edu>
2024-03-05 17:55:12 -0600

    Data Transformation Added

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -3,33 +3,40 @@ from datetime import date
 
 DATABASE_NAME = "US_VISA"
 
-COLLECTION_NAME = 'visa_data'
+COLLECTION_NAME = "visa_data"
 
-MONGODB_URL_KEY = 'MONGODB_URL'
+MONGODB_URL_KEY = "MONGODB_URL"
 
-PIPELINE_NAME: str ='usvisa'
-ARTIFACT_DIR: str = 'artifact'
+PIPELINE_NAME: str = "usvisa"
+ARTIFACT_DIR: str = "artifact"
 
-MODEL_FILE_NAME = 'model.pkl'
+MODEL_FILE_NAME = "model.pkl"
 
-TARGET_COLUMN = 'case_status'
+TARGET_COLUMN = "case_status"
 CURRENT_YEAR = date.today().year
-PREPROCESSING_OBJECT_FILE_NAME = 'preprocessing.pkl'
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 
-FILE_NAME: str = 'usvisa.csv'
-TRAIN_FILE_NAME: str = 'train.csv'
-TEST_FILE_NAME: str = 'test.csv'
+FILE_NAME: str = "usvisa.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "us-east-1"
+
+
 """
-Data Ingestion related constants start with DATA_INGESTION VAR NAME
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
-DATA_INGESTION_COLLECTION_NAME: str = 'visa_data'
-DATA_INGESTION_DIR_NAME: str = 'data_ingestion'
-DATA_INGESTION_FEATURE_STORE_DIR: str = 'feature_store'
-DATA_INGESTION_INGESTED_DIR: str = 'ingested'
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+
 
 """
 Data Validation realted contant start with DATA_VALIDATION VAR NAME
